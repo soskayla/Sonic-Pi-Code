566a5e4320ee0025cfe4f70b74bc3d7d9d6b21d9
soskayla <makayla_washington@student.davincischools.org>
2025-03-03 10:20:40 -0800

    Update Custom Song Code.rb

--- Custom Song Code.rb
+++ Custom Song Code.rb
@@ -1,4 +1,4 @@
-use_bpm 160
+use_bpm 150
 use_synth :piano
 
 #pink + white
@@ -6,26 +6,19 @@ use_synth :piano
 bird = "C:/Users/makayla_washington/Downloads/birds.wav"
 
 define :chords do
-  play :b3, sustain: 2, amp: 0.2
-  play :e4, sustain: 2, amp: 0.2
-  play :g4, sustain: 2, amp: 0.2
-  sleep 2
-  play :a3, sustain: 2, amp: 0.2
-  play :c4, sustain: 2, amp: 0.2
-  play :e4, sustain: 2, amp: 0.2
-  sleep 2
-  play :g3, sustain: 2, amp: 0.2
-  play :b3, sustain: 2, amp: 0.2
-  play :e4, sustain: 2, amp: 0.2
-  sleep 2
-  play :a3, sustain: 2, amp: 0.2
-  play :c4, sustain: 2, amp: 0.2
-  play :e4, sustain: 2, amp: 0.2
-  sleep 2
+  play :e4, sustain: 4, amp: 0.09, sustain: 2
+  sleep 1
+  play :e4, sustain: 4, amp: 0.09, sustain: 2
+  sleep 1
+  play :e4, sustain: 4, amp: 0.09, sustain: 2
+  sleep 1
+  play :e4, sustain: 4, amp: 0.09, sustain: 2
+  sleep 1
+  
 end
 
 live_loop :birds do
-  sample bird, amp: 0.1
+  sample bird, amp: 0.01
   sleep 16
 end
 
@@ -38,7 +31,7 @@ end
 sleep 4
 
 live_loop :melody do
-  play :e4, sustain: 1, amp: 0.2
+  play :e4, sustain: 1, amp: 0.1
   sleep 0.5
   play :g4, sustain: 1, amp: 0.2
   sleep 0.5
@@ -82,3 +75,4 @@ live_loop :ending do
   sleep 4
   stop
 end
+
