26300702e4079e8a0c148c8aa0c55e79de8c1f6a
soskayla <makayla_washington@student.davincischools.org>
2025-03-19 04:11:40 -0700

    Custom Song Code.rb

--- Custom Song Code.rb
+++ Custom Song Code.rb
@@ -1,63 +1,13 @@
-use_bpm 150
+use_bpm 120
 use_synth :piano
 
-#pink + white
-
-define :changeNotesnSleep do |note|
-  play note
-end
-
-bird = "C:/Users/makayla_washington/Downloads/birds.wav"
-
-define :chords do
-  play :e4, sustain: 4, amp: 0.09, sustain: 4
-  sleep 1
-  play :e4, sustain: 4, amp: 0.09, sustain: 4
-  sleep 1
-  play :e4, sustain: 4, amp: 0.09, sustain: 4
-  sleep 1
-  play :e4, sustain: 4, amp: 0.09, sustain: 4
-  sleep 1
-  
-end
+birds = "C:/Users/makayla_washington/Downloads/birds.wav"
 
 live_loop :birds do
-  sample bird, amp: 0.01
+  sample birds, amp: 0.1, rate: 0.8
   sleep 16
 end
-
-sleep 4
-
-live_loop :chords_loop do
-  chords
-end
-
-sleep 4
-
-live_loop :melody do
-  play :e4, sustain: 1, amp: 0.2
-  sleep 0.5
-  play :g4, sustain: 1, amp: 0.2
-  sleep 0.5
-  play :b4, sustain: 1, amp: 0.2
-  sleep 0.5
-  play :e5, sustain: 1, amp: 0.2
-  sleep 1
-  sleep 2
-  play :d5, sustain: 1, amp: 0.2
-  sleep 0.5
-  play :b4, sustain: 1, amp: 0.2
-  sleep 0.5
-  play :g4, sustain: 1, amp: 0.2
-  sleep 0.5
-  play :e4, sustain: 1, amp: 0.2
-  sleep 1
-  sleep 2
-end
-
-
-sleep 4
-
+2
 live_loop :drums do
   sample :drum_cymbal_soft, amp: 0.1
   sleep 1
@@ -69,8 +19,107 @@ live_loop :drums do
   sleep 1
 end
 
+
+live_loop :chords do
+  play :e3, amp: 0.2, release: 4
+  sleep 4
+  play :d3, amp: 0.2, release: 4
+  sleep 4
+  play :c3, amp: 0.2, release: 4
+  sleep 4
+  play :b2, amp: 0.2, release: 4
+  sleep 4
+end
+
+live_loop :melody do
+  play :e4, amp: 0.3, release: 1
+  sleep 1
+  play :g4, amp: 0.3, release: 1
+  sleep 1
+  play :b4, amp: 0.3, release: 1
+  sleep 1
+  play :e5, amp: 0.3, release: 1
+  sleep 1
+  play :d5, amp: 0.3, release: 1
+  sleep 1
+  play :b4, amp: 0.3, release: 1
+  sleep 1
+  play :g4, amp: 0.3, release: 1
+  sleep 1
+  play :e4, amp: 0.3, release: 1
+  sleep 1
+end
+
+live_loop :pad do
+  use_synth :hollow
+  play :e3, amp: 0.1, release: 8
+  sleep 8
+  play :g3, amp: 0.1, release: 8
+  sleep 8
+end
+
+
 sleep 32
 
-stop
+live_loop :chords do
+  stop
+end
 
+live_loop :melody do
+  stop
+end
+
+live_loop :pad do
+  stop
+end
+
+
+sleep 16
+
+live_loop :chords do
+  play :e3, amp: 0.2, release: 4
+  sleep 4
+  play :d3, amp: 0.2, release: 4
+  sleep 4
+  play :c3, amp: 0.2, release: 4
+  sleep 4
+  play :b2, amp: 0.2, release: 4
+  sleep 4
+end
+
+live_loop :melody do
+  play :e4, amp: 0.3, release: 1
+  sleep 1
+  play :g4, amp: 0.3, release: 1
+  sleep 1
+  play :b4, amp: 0.3, release: 1
+  sleep 1
+  play :e5, amp: 0.3, release: 1
+  sleep 1
+  play :d5, amp: 0.3, release: 1
+  sleep 1
+  play :b4, amp: 0.3, release: 1
+  sleep 1
+  play :g4, amp: 0.3, release: 1
+  sleep 1
+  play :e4, amp: 0.3, release: 1
+  sleep 1
+end
+
+live_loop :pad do
+  use_synth :hollow
+  play :e3, amp: 0.1, release: 8
+  sleep 8
+  play :g3, amp: 0.1, release: 8
+  sleep 8
+end
+
+live_loop :fade_out do
+  8.times do |i|
+    set_volume! 1.0 - (i * 0.01)
+    sleep 2
+  end
+  stop
+end
+stop
 
